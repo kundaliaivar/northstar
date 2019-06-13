341703a0b04309ff22954e56f243dbcb67803db4
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2019-06-13 12:28:38 +0530

    Initial api flows and UI structure

--- server/controllers/login.controller.js
+++ server/controllers/login.controller.js
@@ -1,7 +1,19 @@
-const UserModel = require('../models/userModel');
+const User = require('../models/userModel');
 
-exports.test = function(req, res){
-  res.send("Hello, its working! i am from user controller :)");
+
+exports.login = function (req, res) {
+
+  // var user = new User({
+  User.find({
+      userName: req.body.userName,
+      password: req.body.password
+    }).then(response => {
+      if (response.length)
+        res.send(true);
+      else
+        res.send(false);
+    })
+    .catch(err => {
+      res.send(err.message);
+    })
 }
-
-
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const user_controller = require('../controllers/login.controller');
 const goal_controller = require('../controllers/goal.controller');
 
-router.get('/', user_controller.test);
+router.post('/login', user_controller.login);
 router.get('/getGoals', goal_controller.getGoal);
 module.exports = router;
