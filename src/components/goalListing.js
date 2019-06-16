087618a15ec5368d73f4b002bdee91000afa2c45
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2019-06-16 20:34:21 +0530

    added progress circle in goal listing page

--- src/components/goalListing.js
+++ src/components/goalListing.js
@@ -93,8 +93,9 @@ const expData =[
         isHighImpact : 'no',
         isPublic : 'no',
         isCompleted : 'no',
-        percentage : '99%',
-        expDay:161
+        percentage : '30',
+        expDay:161,
+        daysRemaining:-100
     },
 ]
 const inProgressGoalData =[
--- src/components/goalListingComponents/goalTemplate.js
+++ src/components/goalListingComponents/goalTemplate.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
 import { Text , View  } from 'react-native';
+import ProgressCircle from 'react-native-progress-circle';
 
 
 class GoalListing extends Component {
+    setColor(daysRemaining){
+        if(daysRemaining < 0){
+          return ("red")
+        }else{
+            return ("pink")
+            
+        }
+    }
     render() {
         return (
+            <View style = {styles.goalTemplateStyle}>
             <View style = {styles.contaierStyle}>
                 <Text style  = {styles.taskName}>{this.props.data[0].name}</Text>
                 <View style  = {styles.statusStyle}>
-                <Text>{this.props.data[0].percentage}</Text>
-                <Text>exp by {this.props.data[0].expDay} days</Text>
+                <Text>{this.props.data[0].percentage} % |</Text>
+                <Text>Exp by {this.props.data[0].expDay} days</Text>
                 </View>
-                
             </View>
+            <ProgressCircle percent={this.props.data[0].percentage} radius={20} borderWidth={8} color={this.setColor(this.props.data[0].daysRemaining)}  shadowColor='#fafafa' bgColor="#fff"></ProgressCircle>
+            </View>
+            
           
         );
     }
 }
 const styles = {
+    goalTemplateStyle:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        margin:20,
+        opacity:0.5
+    },
+    
     contaierStyle:{
         flexDirection:'column',
-        margin:20
     },
     statusStyle:{
         flexDirection:'row',
