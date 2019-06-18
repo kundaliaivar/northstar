364f354302f586043da7ef6dcf9bf2594a1d6383
Suprita S T <suprita@moonraft.com>
2019-06-18 13:16:24 +0530

    no message

--- App.js
+++ App.js
@@ -14,12 +14,6 @@ import GoalListing from './src/components/goalListing';
 import CreateGoalPage from './src/components/createGoal';
 import GoalDetails from './src/components/goalDetail';
 import LoginPage from './src/components/login/login';
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
 import GoalLandingDetail from './src/components/goalLandingDetail';
 
 export const user = {};
